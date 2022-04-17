875a7b7f7fe294a3675ba36596215a6a1a9f3110
JEONG99 <jbh990621@gmail.com>
2022-04-17 21:05:45 +0900

    feat: dfa, literal_string

--- lexical/dfa.py
+++ lexical/dfa.py
@@ -1,6 +1,12 @@
+import string
+
 WHITESPACE = [" ", "\t", "\0", "\n"]
 OPERATOR = ["+", "-", "*", "/"]
 PROGRAM_KEYWORD = [",", ";"]
+NON_ZERO_DIGIT = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+DIGIT = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+ALPHABET_LOWER = list(string.ascii_lowercase)
+ALPHABET_UPPER = list(string.ascii_uppercase)
 
 
 class DFA:
