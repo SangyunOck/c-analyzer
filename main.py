9e81cf14479aa0fe39ba55c24d263124740c70a2
Sangyun Ock <demibun@a41ventures.com>
2022-04-26 17:17:53 +0900

    fix: change input to argv

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import sys  
 from lexical.lexical_analyzer import LexicalAnalyzer
 
 if __name__ == "__main__":
-    print("Input file name: ", end="")
-    filename = input().strip()
+    filename = sys.argv[1]
     lexical_analyzer = LexicalAnalyzer(filename)
     lexical_analyzer.scan_lexemes()
